ec67ee44924e919fee4ab37405d9d16f576a4023
George Kamtziridis <georgekam96@gmail.com>
2021-05-05 17:14:02 +0300

    Added near-miss cases.

--- class imbalance/under_sampling.py
+++ class imbalance/under_sampling.py
@@ -1,4 +1,4 @@
-from imblearn.under_sampling import RandomUnderSampler
+from imblearn.under_sampling import RandomUnderSampler, NearMiss
 from utilities import load_data, logistic_regression
 
 # Loading data
@@ -7,6 +7,27 @@ X, y = load_data()
 # Performing random under-sampling
 rus = RandomUnderSampler(random_state=0)
 X_resampled, y_resampled = rus.fit_resample(X, y)
+
+# Creating and evaluating the model
+logistic_regression(X_resampled, y_resampled)
+
+# Performing NearMiss-1
+near_miss_1 = NearMiss(version = 1)
+X_resampled, y_resampled = near_miss_1.fit_resample(X, y)
+
+# Creating and evaluating the model
+logistic_regression(X_resampled, y_resampled)
+
+# Performing NearMiss-2
+near_miss_2 = NearMiss(version = 2)
+X_resampled, y_resampled = near_miss_2.fit_resample(X, y)
+
+# Creating and evaluating the model
+logistic_regression(X_resampled, y_resampled)
+
+# Performing NearMiss-3
+near_miss_3 = NearMiss(version = 3)
+X_resampled, y_resampled = near_miss_3.fit_resample(X, y)
 
 # Creating and evaluating the model
 logistic_regression(X_resampled, y_resampled)
