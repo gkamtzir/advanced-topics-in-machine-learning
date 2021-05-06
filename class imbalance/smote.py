a98b3a7f76f50e1cf8084423aefffe0303b11982
George Kamtziridis <georgekam96@gmail.com>
2021-05-06 22:29:00 +0300

    Tested multiple neighbors on smote.

--- class imbalance/smote.py
+++ class imbalance/smote.py
@@ -4,9 +4,11 @@ from utilities import load_data, logistic_regression
 # Loading data
 X, y = load_data()
 
-# Performing smote sampling
-smote = SMOTE(random_state = 0)
-X_resampled, y_resampled = smote.fit_resample(X, y)
-
-# Creating and evaluating the model
-logistic_regression(X_resampled, y_resampled)
+for neighbors in range(5, 50, 5):
+    print(f"Neighbors: {neighbors}")
+    # Performing smote sampling
+    smote = SMOTE(k_neighbors = neighbors, n_jobs = -1, random_state = 0)
+    X_resampled, y_resampled = smote.fit_resample(X, y)
+    
+    # Creating and evaluating the model
+    logistic_regression(X_resampled, y_resampled)
