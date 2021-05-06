b0867c93ed8cd5dc6bed291f19e608d8cf48aead
George Kamtziridis <georgekam96@gmail.com>
2021-05-06 22:57:08 +0300

    Tested multiple neighbors on adasyn.

--- class imbalance/adasyn.py
+++ class imbalance/adasyn.py
@@ -4,9 +4,11 @@ from utilities import load_data, logistic_regression
 # Loading data
 X, y = load_data()
 
-# Performing ADASYN sampling
-adasyn = ADASYN(random_state = 0)
-X_resampled, y_resampled = adasyn.fit_resample(X, y)
-
-# Creating and evaluating the model
-logistic_regression(X_resampled, y_resampled)
+for neighbors in range(5, 50, 5):
+    print(f"Neighbors: {neighbors}")
+    # Performing ADASYN sampling
+    adasyn = ADASYN(n_neighbors = neighbors, n_jobs = -1, random_state = 0)
+    X_resampled, y_resampled = adasyn.fit_resample(X, y)
+    
+    # Creating and evaluating the model
+    logistic_regression(X_resampled, y_resampled)
